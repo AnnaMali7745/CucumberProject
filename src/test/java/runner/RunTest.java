9a5d3a08641fa27ca9e299397845284c7657ac5c
annamali <annaapmali@gmail.com>
2024-01-31 21:43:09 +0530

    by anna first commit

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions
              (
             	 features="src/test/resources/Business_Logic",	 
-            	 tags="@RegressionTest",
+            	 
             	 glue="map",
             	 plugin="pretty",
             	 monochrome=true
